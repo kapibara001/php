36114afcec8e7f207783f725f7d80c3f249bae8f
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-04-26 23:27:45 +0500

    Add files via upload

---
+++ HomeWorks/3004Введение/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // 1 Задание
+        $name = "Maxim";
+        echo "Hello, \"" . $name . "\"!";
+
+
+        // 2 Задание
+        $age = 15;
+        echo "<br>I'm " . $age . " y.o";
+
+        // 3 Задание
+        $a = 15;
+        $b = 414;
+        $result = $a + $b;
+        
+        echo "<br>$a + $b = $result";
+
+        // 4 Задание
+
+    ?>
+</body>
+</html>
